6d947546e75fcf3e4711640a42b287882ee00f7d
Leedasamurai <lesegoleemac@gmail.com>
2024-08-21 17:31:00 +0200

    connecting server to client

--- client/src/components/CustomMessageForms/MessageFormUI.jsx
+++ client/src/components/CustomMessageForms/MessageFormUI.jsx
@@ -2,7 +2,6 @@ import {
   PaperAirplaneIcon,
   PaperClipIcon,
   XCircleIcon,
-  XMarkIcon,
 } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
 import Dropzone from "react-dropzone";
